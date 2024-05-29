b6d36067fef71a24114527181ea96dba3061fb7e
AYHAN GÜNGÖR <gngr.ayhan@gmail.com>
2024-05-29 09:39:24 +0300

    useSpeechRecognition added

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,20 @@ const express = require("express")
 const dbConnect = require("./db")
 const App = require("./app")
 const {PORT} = require("./config")
+const path =  require("path")
 
 async function StartServer() {
     const app = express()
-    
+    const __dirname = path.resolve();
     await dbConnect()
 
     await App(app)
+
+      
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+    })
 
     app.listen(PORT, () => {
         console.log("server is running on port 8000")
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,35 +1,56 @@
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect, useState } from "react"
 import { getVocabularys } from "../features/vocabulary/vocabularySlice"
-import { useSpeechSynthesis } from "react-speech-kit"
+import { useSpeechSynthesis, useSpeechRecognition } from "react-speech-kit"
 const Home = () => {
     const dispatch = useDispatch()
     const { speak, voices } = useSpeechSynthesis()
-    
-     console.log(voices)
-    
+    const [value, setValue] = useState('');
+    const { listen, listening, stop } = useSpeechRecognition({
+        onResult: (result) => {
+            setValue(result );
+        },
+    });
+
+    //  console.log(voices)
+
 
     useEffect(() => {
         dispatch(getVocabularys())
     }, [])
 
     const vocabulary = useSelector((state) => state.vocabulary.vocabularys)
-    
+
     // function handleSpeak() {
     //     speak({ text: text, rate: 0.7, voice: voices[voiceIndex] })
     // }
 
-    const handleSpeakText=(textItem)=> {
-       
-        speak({ text: textItem, rate: 0.7, voice: voices[5] })
+    const handleSpeakText = (textItem) => {
+
+        speak({ text: textItem, rate: 0.6, voice: voices[5] })
     }
 
-   
 
 
-   
+
+
     return (
         <>
+            <div style={{left:0,position:'fixed'}}>
+                <textarea
+                    value={value}
+                    onChange={(event) => setValue(event.target.value)}
+                    rows='4'
+                    cols='40'
+                />
+                {/* ({interimResults: true,lang:"en-US"}) */}
+                <br/>
+                <button onMouseDown={()=>listen({lang:'en-US'})} onMouseUp={stop}>
+                    🎤
+                </button><br/>
+                <button onClick={()=>setValue('')}>Clear</button>
+                {listening && <div>Go ahead Im listening</div>}
+            </div>
             {/* <textarea
                 rows='5'
                 cols='50'
@@ -60,48 +81,51 @@ const Home = () => {
                 // boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
                 // marginBottom: '20px',
             }}>
-            {vocabulary.map((item, index) => {
-                return(
-                    <div style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        backgroundColor: 'lightgray',
-                        padding: '20px',
-                        borderRadius: '8px',
-                        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-                        marginBottom: '20px',
-                    }} key={index}>
-                    <ul>
-                        <li>{item.vocabulary}</li>
-                        <li>
-                            <ol>
-                              {item.text.map((text,index)=>{  
-                            return(
-                                <li key={index}>{text.text}<button onClick={()=>handleSpeakText(text.text)}>Speak</button><br/>
-                                
-                                {text.textVocabulary.map((textVocabulary,index)=>{
-                                    return (
-                                        <div key={index}>
-                                        <span>{textVocabulary.vocabulary}:{textVocabulary.mean}</span>
-                                    </div>
-                                    )
-                                })}
-                                {text.textTranslate.map((translate,index)=>{
-                                   return (
-                                    <div key={index}>
-                                    <span>{translate.textTranslate}</span>
-                                </div>
-                                   )
-                                })}
-                                </li>
-                                
-                            )
-                        })}</ol></li>
-                    </ul>
-                </div>
-                )
-            })}
-        </div >
+
+                {vocabulary.map((item, index) => {
+                    return (
+                        <div style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            backgroundColor: 'lightgray',
+                            padding: '20px',
+                            borderRadius: '8px',
+                            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+                            marginBottom: '20px',
+                        }} key={index}>
+                            <ul style={{ listStyle: "none" }}>
+                                <li>{item.vocabulary} : {item.mean}</li>
+                                <li>
+                                    <ol>
+                                        {item.text.map((text, index) => {
+                                            return (
+                                                <li key={index}>{text.text}<button onClick={() => handleSpeakText(text.text)}>Listen</button>
+                                                    <br />
+
+                                                    {text.textVocabulary.map((textVocabulary, index) => {
+                                                        return (
+                                                            <div key={index}>
+                                                                <span>{textVocabulary.vocabulary}:{textVocabulary.mean}</span>
+                                                            </div>
+                                                        )
+                                                    })}
+                                                    {text.textTranslate.map((translate, index) => {
+                                                        return (
+                                                            <div key={index}>
+                                                                <span>{translate.textTranslate}</span>
+                                                                <hr />
+                                                            </div>
+                                                        )
+                                                    })}
+                                                </li>
+
+                                            )
+                                        })}</ol></li>
+                            </ul>
+                        </div>
+                    )
+                })}
+            </div >
         </>
     )
 }
